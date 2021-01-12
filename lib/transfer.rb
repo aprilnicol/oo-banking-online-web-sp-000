03e0f36c909732c10a8b7dd22aa7f0ff7d8bc919
April Adams <illusionlace@gmail.com>
2021-01-12 15:06:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,15 +24,15 @@ class Transfer
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
-  end 
+  end
   def reverse_transfer
     if valid? && receiver.balance > amount && self.status == "complete"
       sender.deposit(self.amount)
       receiver.withdrawal(self.amount)
       self.status = "reversed"
-    else 
+    else
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
-    end 
-  end 
+    end
+  end
 end 
