e30f60031da59811a904e033014b1187652da75a
April Adams <illusionlace@gmail.com>
2021-01-12 15:04:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,19 +16,15 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance < @amount
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit(@amount * -1)
-      @receiver.deposit(@amount)
-      @status = "complete"
-    end
-  end
-
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.withdrawal(self.amount)
+      receiver.deposit(self.amount)
+      self.status = "complete"
+    else 
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end 
+  end 
   def reverse_transfer
     if @status == "complete"
       @sender.deposit( @amount )
