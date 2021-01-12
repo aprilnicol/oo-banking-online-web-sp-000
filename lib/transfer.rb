5416d54392f6c14b57459e8d8699598143d1b1e6
April Adams <illusionlace@gmail.com>
2021-01-12 14:51:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   attr_accessor :sender, :receiver, :amount, :status, :count
   def initialize?(sender, receiver, amount)
     @sender = sender
-    @reciever = receiver
+    @receiver = receiver
     @status = "pending"
 end
 def both_valid?
